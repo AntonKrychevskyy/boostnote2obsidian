f2c6b89e8b8635a6dcc49ed3c9529577120f2532
Anton Krychevskyy <Anton.Krychevskyy@gmail.com>
2023-03-30 17:13:49 +0300

    Adds functionality to track all attachments names in a scope of folder and avoid name collisions, and similar functionality for note names. Adds more descriptive context to console warnings.

--- src/boostnote2obsidian.ts
+++ src/boostnote2obsidian.ts
@@ -36,7 +36,7 @@ interface BoostnoteFolder extends BoostnoteEntity {
   orderedIds: string[];
 }
 
-type BoostnoteFoldersMap = Map<string, BoostnoteFolder>;
+type BoostnoteFoldersMap = Record<string, BoostnoteFolder>;
 
 interface BoostnoteNote extends BoostnoteEntity {
   folderPathname: string;
@@ -47,9 +47,20 @@ interface BoostnoteNote extends BoostnoteEntity {
   _rev: string;
 }
 
-type NotesMap = Map<string, NoteFoldersData>;
+type FoldersMap = Map<string, FolderFilesData>;
 
-interface NoteFoldersData {
+interface FolderFilesData {
+  outputPath: string;
+  attachmentsFolderPath: string;
+  noteIds: string[];
+  noteNames: Map<string, number>;
+  attachmentNameDups: Map<string, number>;
+  attachmentIdsToNames: Map<string, string>;
+}
+
+type NotesUpdateMap = Map<string, NoteUpdateData>;
+
+interface NoteUpdateData {
   relativeFolder: string;
   outputFolder: string;
   filename: string;
@@ -67,7 +78,6 @@ type NoteContentMatchArray = [
   RegExpMatchArray;
 
 interface NoteContentTemplate {
-  title: string;
   tags: string[];
   substrings: string[];
   links: Map<string, ReferenceDataObject>;
@@ -85,6 +95,16 @@ interface ReferenceInstanceObject {
   index: number;
 }
 
+interface NoteUpdateConfig {
+  noteId: string;
+  noteTitle: string;
+  attachmentsFolderPath: string;
+  newFolderPath: string;
+  relativeFolderPath: string;
+  attachmentNameDups: Map<string, number>;
+  attachmentIdsToNames: Map<string, string>;
+}
+
 export default class Boostnote2Obsidian {
   sourcePath: string;
   outputPath: string;
@@ -95,8 +115,8 @@ export default class Boostnote2Obsidian {
   isTitleIncluded: boolean;
   isWikiFormat: boolean;
   attachmentsFolder: string;
-  notesMap: NotesMap = new Map();
-  errors: string[] = [];
+  foldersMap: FoldersMap = new Map();
+  notesMap: NotesUpdateMap = new Map();
 
   constructor(config: Config) {
     this.sourcePath = path.resolve(config.source);
@@ -115,27 +135,41 @@ export default class Boostnote2Obsidian {
 
   async run() {
     // 1. Parse boostnote.json
-    const foldersMap = await this.getBoostnoteFoldersMap();
-    //   1.1 If need to update doc references make Map of all notes
-    this.notesMap = await this.getAllNotesMap(foldersMap);
-    //   1.2 Make list of folders with notes and related notes iterator
-    const [foldersPathsList, notesList] = this.getFoldersAndNotesLists(foldersMap);
-    // 2. Make output folders and notes parsing queue
-    await this.makeOutputFolders(foldersPathsList);
+    //   1.1 Make Map of all folders
+    this.foldersMap = await this.getAllFoldersMap();
+    //   1.2 Make Map of all notes
+    this.notesMap = await this.getAllNotesMap(this.foldersMap);
+    // 2. Get only required folders map
+    this.foldersMap = this.getRequiredFoldersMap(this.foldersMap);
+    // 3. Get only required notes map
+    const requiredNotesMap = this.getRequiredNotesMap(this.notesMap, this.foldersMap);
+    // 4. Create required output folders and attachments subfolders
+    await this.makeOutputFolders(this.foldersMap);
 
-    for await (const noteId of notesList) {
-      // 3. Parse note returning note content template
-      const noteDataObject = await this.getNoteDataObject(noteId);
-
-      if (!noteDataObject) continue;
+    for await (const [noteId, noteUpdateData] of requiredNotesMap) {
+      const folderData = this.foldersMap.get(noteUpdateData.relativeFolder) as FolderFilesData;
+      const noteUpdateConfig: NoteUpdateConfig = {
+        noteId,
+        noteTitle: noteUpdateData.filename,
+        attachmentsFolderPath: folderData.attachmentsFolderPath,
+        newFolderPath: folderData.outputPath,
+        relativeFolderPath: noteUpdateData.relativeFolder,
+        attachmentNameDups: folderData.attachmentNameDups,
+        attachmentIdsToNames: folderData.attachmentIdsToNames,
+      };
+      // 5. Parse note and get note content template
+      const noteDataObject = (await this.getNoteDataObject(
+        noteUpdateConfig,
+        'processing note'
+      )) as BoostnoteNote;
 
       const noteContentTemplate = this.getNoteContentTemplate(noteDataObject);
-      // 4. Update attachments in note content template
-      await this.updateAttachments(noteId, noteContentTemplate);
-      // 5. If needed - update doc references in note content template
-      this.updateDocReferences(noteContentTemplate);
-      // 6. Convert note content template to file content
-      await this.writeNoteFile(noteId, noteContentTemplate);
+      // 6. Update attachments in note content template
+      await this.updateAttachments(noteContentTemplate, noteUpdateConfig);
+      // 7. If needed - update doc references in note content template
+      this.updateDocReferences(noteContentTemplate, noteUpdateConfig);
+      // 8. Convert note content template to file content
+      await this.writeNoteFile(noteContentTemplate, noteUpdateConfig);
     }
 
     // try {
@@ -158,43 +192,102 @@ export default class Boostnote2Obsidian {
     // }
   }
 
-  private async getBoostnoteFoldersMap() {
+  private async getAllFoldersMap() {
     const jsonPath = path.resolve(this.sourcePath, 'boostnote.json');
     // this.ora.text = `Reading ${jsonPath}`;
     const json: string = await fs.readFile(jsonPath, 'utf-8');
     const { folderMap }: BoostnoteRegistry = JSON.parse(json);
 
-    return folderMap;
+    const foldersMap: FoldersMap = Object.entries(folderMap).reduce(
+      (map, [key, { orderedIds }]) => {
+        const noteIds = orderedIds.filter(id => id.startsWith('note:')).map(id => id.slice(5));
+
+        if (!noteIds.length) return map;
+
+        const relativePath = key.slice(1);
+        const outputPath = path.join(this.outputPath, relativePath);
+        const attachmentsFolderPath = path.join(outputPath, this.attachmentsFolder);
+
+        map.set(relativePath, {
+          noteIds,
+          outputPath,
+          attachmentsFolderPath,
+          noteNames: new Map<string, number>(),
+          attachmentNameDups: new Map<string, number>(),
+          attachmentIdsToNames: new Map<string, string>(),
+        });
+
+        return map;
+      },
+      new Map()
+    );
+
+    return foldersMap;
   }
 
-  private async getAllNotesMap(foldersMap: BoostnoteFoldersMap) {
-    const notesMap: NotesMap = new Map();
+  private async getAllNotesMap(foldersMap: FoldersMap) {
+    const notesMap: NotesUpdateMap = new Map();
 
-    for (const { _id, orderedIds } of Object.values(foldersMap)) {
-      const relativeFolder = _id.replace(/^folder\:\//g, '');
-      const outputFolder = path.join(this.outputPath, relativeFolder);
-
-      for (let id of orderedIds) {
-        if (!id.startsWith('note:')) continue;
-
-        const noteDataObject = await this.getNoteDataObject(id.slice(5));
+    for (const [
+      relativeFolder,
+      { noteIds: notes, outputPath: outputFolder, noteNames },
+    ] of foldersMap) {
+      for (let noteId of notes) {
+        const noteDataObject = await this.getNoteDataObject(
+          {
+            noteId,
+            relativeFolderPath: relativeFolder,
+          },
+          'all notes mapping'
+        );
 
         if (!noteDataObject) continue;
 
-        const filename = this.sanitizeTitle(noteDataObject.title);
-        notesMap.set(id.slice(5), { outputFolder, relativeFolder, filename });
+        const title = this.sanitizeTitle(noteDataObject.title);
+        const count = noteNames.has(title) ? noteNames.get(title)! : 0;
+        const filename = count ? `${title}(${count})` : title;
+        noteNames.set(title, count + 1);
+
+        notesMap.set(noteId, { outputFolder, relativeFolder, filename });
       }
     }
 
     return notesMap;
   }
 
-  private async getNoteDataObject(noteId: string) {
+  private getRequiredFoldersMap(foldersMap: FoldersMap) {
+    return new Map([...foldersMap].filter(([relativePath]) => this.isRequiredFolder(relativePath)));
+  }
+
+  private isRequiredFolder(folderPath: string) {
+    if (this.isAll) return true;
+
+    const folderPathSegments = folderPath.split('/');
+
+    return (
+      (!this.includeFolders.length ||
+        !!this.includeFolders.some(folder => folderPathSegments.includes(folder))) &&
+      (!this.excludeFolders.length ||
+        !this.excludeFolders.some(folder => folderPathSegments.includes(folder)))
+    );
+  }
+
+  private getRequiredNotesMap(notesMap: NotesUpdateMap, requiredFoldersMap: FoldersMap) {
+    return new Map(
+      [...notesMap].filter(([_, { relativeFolder }]) => requiredFoldersMap.has(relativeFolder))
+    );
+  }
+
+  private async getNoteDataObject(
+    { noteId, relativeFolderPath }: Pick<NoteUpdateConfig, 'noteId' | 'relativeFolderPath'>,
+    processDesc: string
+  ) {
     const notePath = path.resolve(this.sourcePath, 'notes', noteId + '.json');
 
     if (!fs.existsSync(notePath)) {
-      console.error(`Note file ${notePath} not found.`);
-      this.errors.push(`Note file ${notePath} not found.`);
+      console.warn(
+        `During ${processDesc} file "${notePath}" from "${relativeFolderPath}" wasn't found.`
+      );
       return null;
     }
 
@@ -203,53 +296,18 @@ export default class Boostnote2Obsidian {
     return JSON.parse(noteDataString) as BoostnoteNote;
   }
 
-  private getFoldersAndNotesLists(foldersMap: BoostnoteFoldersMap) {
-    const foldersList: string[] = [];
-    const notesList: string[] = [];
-
-    for (const { _id, orderedIds } of Object.values(foldersMap) as BoostnoteFolder[]) {
-      const relativePath = _id.split('/').slice(1);
-
-      if (this.isForbiddenFolder(relativePath)) continue;
-
-      const fullPath = path.join(this.outputPath, ...relativePath);
-      const notes = orderedIds.filter(id => id.startsWith('note:')).map(id => id.slice(5));
-
-      if (notes.length) {
-        foldersList.push(fullPath);
-        notesList.push(...notes);
-      }
-    }
-
-    return [foldersList, notesList] as [string[], string[]];
-  }
-
-  private isForbiddenFolder(folderPathSegments: string[]) {
-    if (this.isAll) return false;
-
-    return (
-      (!!this.includeFolders.length &&
-        !this.includeFolders.some(folder => folderPathSegments.includes(folder))) ||
-      (!!this.excludeFolders.length &&
-        this.excludeFolders.some(folder => folderPathSegments.includes(folder)))
-    );
-  }
-
-  private async makeOutputFolders(foldersList: string[]) {
-    for await (const outputFullPath of foldersList) {
+  private async makeOutputFolders(foldersMap: FoldersMap) {
+    for await (const [, { attachmentsFolderPath: attachmentsPath }] of foldersMap) {
       try {
-        const attachmentsFolderPath = path.join(outputFullPath, this.attachmentsFolder);
-        await fs.ensureDir(attachmentsFolderPath);
+        await fs.ensureDir(attachmentsPath);
       } catch (error) {
-        console.error(`Failed to create folder ${outputFullPath}`, error);
-        this.errors.push(`Failed to create folder ${outputFullPath}`);
+        console.error(`Failed to create folder "${attachmentsPath}"`, error);
       }
     }
   }
 
   private getNoteContentTemplate(noteDataObject: BoostnoteNote) {
-    let { title, content, tags } = noteDataObject;
-    title = this.sanitizeTitle(title);
+    let { content, tags } = noteDataObject;
     tags = this.isTagsIncluded ? tags.map(tag => `#${tag}`).concat(['\n\n']) : [];
     content = this.isTitleIncluded ? content : content.replace(TITLE_REGEX, '');
 
@@ -280,36 +338,41 @@ export default class Boostnote2Obsidian {
         : (collection
             .set(reference, { label, items: new Set() })
             .get(reference) as ReferenceDataObject);
+
       refereceData.items.add({ label, title, index: substrings.length - 1 });
     }
 
     if (prevPosition < content.length) substrings.push(content.slice(prevPosition));
 
-    return { title, tags, substrings, links, attachments } as NoteContentTemplate;
+    return { tags, substrings, links, attachments } as NoteContentTemplate;
   }
 
-  private async updateAttachments(noteId: string, noteContentTemplate: NoteContentTemplate) {
+  private async updateAttachments(
+    noteContentTemplate: NoteContentTemplate,
+    noteUpdateConfig: NoteUpdateConfig
+  ) {
     const { substrings, attachments } = noteContentTemplate;
-    const { outputFolder } = this.notesMap.get(noteId) as NoteFoldersData;
-    const attachmentsFolder = path.resolve(outputFolder, this.attachmentsFolder);
+    const { newFolderPath, noteTitle, ...restNoteUpdateConfig } = noteUpdateConfig;
 
-    for await (const [attachmentReference, { label, items }] of attachments) {
+    for await (const [attachmentReference, { label = '', items }] of attachments) {
       const attachmentPath = path.resolve(this.sourcePath, 'attachments', attachmentReference);
 
       if (!fs.existsSync(attachmentPath)) {
-        console.error(`Attachment ${attachmentPath} not found`);
-        this.errors.push(`Attachment ${attachmentPath} not found`);
+        console.warn(
+          `Attachment "${attachmentPath}" referenced in "${noteTitle}" from "${newFolderPath}" wasn't found.`
+        );
         continue;
       }
 
-      const ext = path.extname(attachmentPath);
-      const referenceName = label.replace('/.+w+$/g', '') || attachmentReference.replace(ext, '');
-      const newFilename = this.sanitizeTitle(referenceName).replaceAll(' ', '_') + ext;
-      const newFilePath = path.resolve(attachmentsFolder, newFilename);
+      const { newFilePath, newFilename } = this.calcNewAttachmentLocation({
+        label,
+        attachmentReference,
+        ...restNoteUpdateConfig,
+      });
 
       await fs.copyFile(attachmentPath, newFilePath);
 
-      for (const { index, label, title } of items) {
+      for (const { index, label = '', title } of items) {
         if (this.isWikiFormat) {
           substrings[index] = `![[${newFilename}${label ? `|${label}` : ''}]]`;
         } else {
@@ -319,20 +382,56 @@ export default class Boostnote2Obsidian {
     }
   }
 
-  private updateDocReferences(noteContentTemplate: NoteContentTemplate) {
-    const { substrings, links } = noteContentTemplate;
+  private calcNewAttachmentLocation({
+    label,
+    attachmentReference,
+    attachmentsFolderPath,
+    attachmentIdsToNames,
+    attachmentNameDups,
+  }: { label: string; attachmentReference: string } & Pick<
+    NoteUpdateConfig,
+    'attachmentsFolderPath' | 'attachmentIdsToNames' | 'attachmentNameDups'
+  >) {
+    if (attachmentIdsToNames.has(attachmentReference)) {
+      const newFilename = attachmentIdsToNames.get(attachmentReference) as string;
+      const newFilePath = path.resolve(attachmentsFolderPath, newFilename);
 
-    for (const [linkReference, { items }] of links) {
-      if (!this.notesMap.has(linkReference)) {
-        console.warn(`Linked note ${linkReference} not found`);
-        this.errors.push(`Linked note ${linkReference} not found`);
+      return { newFilePath, newFilename };
+    }
+
+    const ext = path.extname(attachmentReference);
+    const attachmentTitle = label?.replace(/\.+\w+$/g, '') || attachmentReference.replace(ext, '');
+    const duplicate = attachmentNameDups.get(attachmentTitle + ext) ?? 0;
+    const suffix = duplicate ? `(${duplicate})` : '';
+    const newAttachmentName = this.sanitizeTitle(attachmentTitle).replaceAll(' ', '_');
+    const newFilename = newAttachmentName + suffix + ext;
+    const newFilePath = path.resolve(attachmentsFolderPath, newFilename);
+
+    attachmentIdsToNames.set(attachmentReference, newFilename);
+    attachmentNameDups.set(attachmentTitle + ext, duplicate + 1);
+
+    return { newFilePath, newFilename };
+  }
+
+  private updateDocReferences(
+    noteContentTemplate: NoteContentTemplate,
+    noteUpdateConfig: NoteUpdateConfig
+  ) {
+    const { substrings, links } = noteContentTemplate;
+    const { relativeFolderPath, noteTitle } = noteUpdateConfig;
+
+    for (const [noteId, { items }] of links) {
+      if (!this.notesMap.has(noteId)) {
+        console.warn(
+          `Note "${noteId}" referenced in "${noteTitle}" from "${relativeFolderPath}" wasn't found`
+        );
         continue;
       }
 
-      const { relativeFolder, filename } = this.notesMap.get(linkReference) as NoteFoldersData;
+      const { relativeFolder, filename } = this.notesMap.get(noteId) as NoteUpdateData;
       const newFilePath = relativeFolder ? `${relativeFolder}/${filename}` : filename;
 
-      for (const { index, label, title } of items) {
+      for (const { index, label = '', title } of items) {
         if (this.isWikiFormat) {
           substrings[index] = `[[${newFilePath}${label ? `|${label}` : ''}]]`;
         } else {
@@ -342,17 +441,19 @@ export default class Boostnote2Obsidian {
     }
   }
 
-  private async writeNoteFile(noteId: string, noteContentTemplate: NoteContentTemplate) {
-    const { title, tags, substrings } = noteContentTemplate;
+  private async writeNoteFile(
+    noteContentTemplate: NoteContentTemplate,
+    noteUpdateConfig: NoteUpdateConfig
+  ) {
+    const { tags, substrings } = noteContentTemplate;
+    const { newFolderPath, noteTitle } = noteUpdateConfig;
     const content = tags.join(' ') + substrings.join('');
-    const { outputFolder } = this.notesMap.get(noteId) as NoteFoldersData;
-    const filePath = path.resolve(outputFolder, title + '.md');
+    const filePath = path.resolve(newFolderPath, noteTitle + '.md');
 
     try {
       await fs.writeFile(filePath, content, 'utf-8');
     } catch (error) {
-      console.error(`Failed to write ${filePath} file on disk`, error);
-      this.errors.push(`Failed to write ${filePath} file on disk`);
+      console.error(`Failed to write "${filePath}" file to disk`, error);
     }
   }
 
